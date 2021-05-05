5cc097291089bbdd0e897c989db8f3fc495f9510
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-04 20:28:30 -0500

    doing server work

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -18,24 +18,4 @@ const Home = () => {
 }
 
 
-import React from "react"
-const Home = () => {
-
-  return (
-    <div className="container home-container" id="home-title">
-      <div className="row">
-        <div className="jumbotron home-title">
-        <h1>Tweet Hunter</h1>
-        <p className="lead">Welcome to the Tweet Hunter!</p>
-        <hr className="my-4"></hr>
-        <p>
-          This website can be used to locate tweets utilizing the Twitter API. Use the navigation bar to look for tweets from your favorite users. The "Random Tweet" page will display random tweets from 5 of my favorite Twitter accounts. Feel free to use the random tweet button to display a random tweet from these users or simply go back to the "Search" page to find any user your heart desires. Happy Hunting! 
-        </p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-
 export default Home
