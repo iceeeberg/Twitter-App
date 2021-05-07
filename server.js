d70766573564d150e00e3e049b6cb6780bf1f1bf
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-06 20:37:35 -0500

    dummy server completed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 const path = require('path');
 const axios = require('axios');
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config()
 const token = process.env.BEARER_TOKEN;
 
