0b9cba425b6b27ef92885d13e4ede409c6c0b157
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-08 14:27:40 -0500

    working on search users

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -1,12 +1,40 @@
-import React from "react"
+import React, {useState} from "react";
+import axios from 'axios';
 
-const Search = () => {
+const Search = ({ user }) => {
+  const [search, setSearch] = useState("");
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (search === "") {
+      alert("You must enter a Twitter user!")
+    } else {
+      axios
+        .get('/api/tweets/users')
+        .then(res => {
+          setSearch(user = () => {
+            return res.data.username
+          });
+          console.log(res.data.username)
+        });
+    };
+  };
 
   return (
     <div>
-      <input></input>
+      <input 
+        placeholder="Search Username..."
+        type="text"
+        name="user"
+        onChange={(e) => setSearch(e.target.value)}
+        >
+      </input>
         <br></br>
-      <button className="btn btn-primary mb-2">Search Tweet</button>
+      <button 
+        className="btn btn-primary mb-2"
+        onSubmit={handleSubmit}
+      >Search Tweet
+      </button>
     </div>
   )
 }
