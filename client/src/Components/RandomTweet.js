81a619f5f44255030784980eac801f966d48f26e
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-10 20:50:41 -0500

    added users to random tweet

--- client/src/Components/RandomTweet.js
+++ client/src/Components/RandomTweet.js
@@ -9,23 +9,23 @@ import nintendoImage from '../images/nintendo.png';
 const RandomTweet = () => {
 
   return (
-    <div>
-      <div>
+    <div className="row">
+      <div className="col-sm-3">
         <figure>
-        <img className="image" src={billgatesImage}></img>
-        <img className="image" src={elonmuskImage}></img>
-        <img className="image" src={lebronjamesImage}></img>
-        <img className="image" src={nbaImage}></img>
-        <img className="image" src={nintendoImage}></img>
+          <img className="img-thumbnail" src={billgatesImage}></img>
+          <img className="img-thumbnail" src={elonmuskImage}></img>
+          <img className="img-thumbnail" src={lebronjamesImage}></img>
+          <img className="img-thumbnail" src={nbaImage}></img>
+          <img className="img-thumbnail" src={nintendoImage}></img>
         </figure>
       </div>
-    <div>
-      <button className="btn btn-primary mb-2">Random Tweet</button>
-      <button className="btn btn-primary mb-2">Random Tweet</button>
-      <button className="btn btn-primary mb-2">Random Tweet</button>
-      <button className="btn btn-primary mb-2">Random Tweet</button>
-      <button className="btn btn-primary mb-2">Random Tweet</button>
-    </div>
+      <div>
+        <button className="btn btn-dark mb-2">Bill Gates</button>
+        <button className="btn btn-dark mb-2">Elon Musk</button>
+        <button className="btn btn-dark mb-2">Lebron James</button>
+        <button className="btn btn-dark mb-2">NBA</button>
+        <button className="btn btn-dark mb-2">Nintendo</button>
+      </div>
     </div>
   )
 }
