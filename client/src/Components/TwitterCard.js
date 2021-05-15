c65357764d1e14faa1abb425aeb7f7cf1344a57b
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-05-15 13:07:13 -0500

    tweet card added

--- client/src/Components/TwitterCard.js
+++ client/src/Components/TwitterCard.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Card } from "react-bootstrap";
-import twitterHeart from '../images/twitter_heart.png';
+import twitterHeart from '../images/twitter_heart.jpg'
 import twitterRetweet from '../images/retweet.png';
 
 const TwitterCard = ({tweet}) => {
 
   return (
-    <Card key={tweet.id} className="bg-dark text-white">
+    <Card id='tweet-card' key={tweet.id} className="bg-dark text-white">
+      <Card.Header>
+        <Card.Img className='rounded-circle' variant='top' src={tweet.user.profile_image_url}
+          style={{ height: 70, width: 70 }} />
+      </Card.Header>
       <Card.Body>
-        <Card.img variant="top" src={tweet.user.profile_image_url}></Card.img>
         <Card.Title>@{tweet.user.screen_name}</Card.Title>
         <Card.Text>{tweet.text}</Card.Text>
       </Card.Body>
       <Card.Footer>
         <small className="icon">
-          <img src={twitterRetweet}></img>
+          <img src={twitterRetweet} style={{height: 20, width: 25}} ></img>
             {tweet.retweet_count}
         </small>
         <small className="icon">
-          <img src={twitterHeart}></img>
+          <img src={twitterHeart}  style={{height: 20, width: 25}} ></img>
           {tweet.favorite_count}
         </small>
       </Card.Footer>
